a3d4cad29bc9f20f773a9dab910a325326eacf93
dagregi <dagim6066@gmail.com>
2024-07-25 10:15:57 +0300

    oop

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-def read_file(book_path):
-    book = open(book_path, "r")
-    text = book.read()
-    book.close()
+class Book:
+    def __init__(self, book_path):
+        book = open(book_path, "r")
+        self.text = book.read()
+        self.word_count = self.get_word_count()
+        self.chars = self.get_character_count()
+        self.ch_list = self.get_character_list()
+        book.close()
 
-    return text
+    def get_word_count(self):
+        word_list = self.text.split()
+        return len(word_list)
 
+    def get_character_count(self):
+        chars = {}
+        for ch in self.text:
+            ch_l = ch.lower()
+            if ch_l in chars:
+                chars[ch_l] += 1
+            else:
+                chars[ch_l] = 1
+        return chars
 
-def get_word_count(text):
-    word_list = text.split()
-    return len(word_list)
+    def get_character_list(self):
+        ch_list = []
 
+        for c in self.chars:
+            if c.isalpha():
+                ch_list.append({"char": c, "count": self.chars[c]})
 
-def get_character_count(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
-
-
-def placeholder(character_dict):
-    ch_list = []
-
-    for c in character_dict:
-        if c.isalpha():
-            ch_list.append({"char": c, "count": character_dict[c]})
-
-    ch_list.sort(reverse=True, key=sort_on)
-    return ch_list
+        ch_list.sort(reverse=True, key=sort_on)
+        return ch_list
 
 
 def sort_on(dict):
@@ -39,13 +38,11 @@ def sort_on(dict):
 
 def main():
     book_path = "books/frankenstein.txt"
-    book = read_file(book_path)
-    word_count = get_word_count(book)
-    char_list = placeholder(get_character_count(book))
+    book = Book(book_path)
 
     print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in the document\n")
-    for item in char_list:
+    print(f"{book.word_count} words found in the document\n")
+    for item in book.ch_list:
         print(f"The '{item["char"]}' character was found {item["count"]} times")
     print("--- End report ---")
 
